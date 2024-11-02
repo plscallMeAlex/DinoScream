47d5ac196372ea31862ce950aa36806e6cd932e9
plscallMeAlex <alexkung007@gmail.com>
2024-11-03 01:08:35 +0700

    fixing obstacles

--- src/gamescene/Gameplay.py
+++ src/gamescene/Gameplay.py
@@ -1,8 +1,10 @@
 import pygame
+import random
 from src.gamescene.GameState import GameState
 from src.Dino import Dino
 from src.Tile import Tile
 from src.obstacles.Cactus import Cactus
+from src.obstacles.Bird import Bird
 from src.modules.Mpu_6050_md import get_tilt_angle
 from src.modules.KY_037_md import JUMP_EVENT, CROUCH_EVENT
 
@@ -13,10 +15,18 @@ class Gameplay(GameState):
 
         self.__dino = Dino()
         self.__tile = Tile(self.__dino.rect)
-        self.__cactus = Cactus(10)
+        self.__obstacles = []
+        self.__obstacles_last_spawn = 0
+
         pygame.display.set_caption("DinoScream")
 
     def update(self, delta_time, screen):
+        # Randomly spawn obstacles
+        self.__obstacles_last_spawn += delta_time
+        if self.__obstacles_last_spawn >= 2000:
+            self.random_obstacle()
+            self.__obstacles_last_spawn = 0
+
         """
         Dino update function
         param: screen_width: width of the screen for range of movement
@@ -27,13 +37,18 @@ class Gameplay(GameState):
             tilt_angle=get_tilt_angle(),
             elapsed_time=delta_time,
         )
-        self.__cactus.update()
+
+        # Update the obstacles objects
+        for obj in self.__obstacles:
+            obj.update()
 
     def render(self, screen):
         screen.fill((255, 255, 255))  # filling the background to white
         self.__dino.draw(screen)
         self.__tile.draw(screen)
-        self.__cactus.draw(screen)
+
+        for obj in self.__obstacles:
+            obj.draw(screen)
 
     def handle_event(self, events):
         for event in events:
@@ -45,6 +60,17 @@ class Gameplay(GameState):
                 self.__dino.jump()
             elif event.type == CROUCH_EVENT:
                 self.__dino.crouch()
+
+    def random_obstacle(self):
+        rand = random.randint(0, 5)
+        if rand <= 2:
+            print("Cactus Spawn")
+            cactus = Cactus(20)
+            self.__obstacles.append(cactus)
+        else:
+            print("Bird Spawn")
+            bird = Bird()
+            self.__obstacles.append(bird)
 
     def run(self, delta_time, screen, events):
         self.handle_event(events)
--- src/obstacles/Cactus.py
+++ src/obstacles/Cactus.py
@@ -1,13 +1,14 @@
 import pygame
 from ..Configuration import Setting
 
+
 class Cactus(pygame.sprite.Sprite):
     def __init__(self, size, speed=-5):
         super().__init__()
 
         # placeholder imgage
         self.image = pygame.image.load("resources/cactus.png")
-        self.rect = pygame.Rect(Setting.screen_height+100, 400, size, 47)
+        self.rect = pygame.Rect(Setting.screen_height + 100, 400, size, 47)
         self.x_velocity = speed
 
     def draw(self, screen):
@@ -15,7 +16,8 @@ class Cactus(pygame.sprite.Sprite):
 
     def update(self):
         self.rect.x += self.x_velocity
-        self.check_out_of_screen()        
+        if self.check_out_of_screen():
+            self.kill()
 
     def kill(self):
         super().kill()
@@ -25,7 +27,3 @@ class Cactus(pygame.sprite.Sprite):
         if self.rect.right < -20:
             return True
         return False
-
-
-
-    
