1b068bb461d98e4086c358c9fd94f2e1c64ea508
plscallMeAlex <alexkung007@gmail.com>
2024-11-08 17:24:54 +0700

    remove audio

--- src/Configuration.py
+++ src/Configuration.py
@@ -7,9 +7,6 @@ class Setting:
     screen_width = 800  # default screen width
     screen_height = 600  # default screen height
 
-    audio_volume = 1.0
-    audio_mute = False
-
     # Screen settings
     def getVideoMode(self):
         if self.fullscreen:
